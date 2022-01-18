a69732ae2e2f7e63c035089f9c8a61db89e8f8e2
kamyshenJava <kamyshen@tutanota.com>
2022-01-18 19:12:45 +0300

    4.6.2. Switch - matcher [#334071]

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -5,11 +5,11 @@ public class MultipleSwitchWeek {
         return switch (name) {
             case "Понедельник", "Monday" -> 1;
             case "Вторник", "Tuesday" -> 2;
-            case "Среда", "Wednesday" -> 2;
-            case "Четверг", "Thursday" -> 2;
-            case "Пятница", "Friday" -> 2;
-            case "Суббота", "Saturday" -> 2;
-            case "Воскресенье", "Sunday" -> 2;
+            case "Среда", "Wednesday" -> 3;
+            case "Четверг", "Thursday" -> 4;
+            case "Пятница", "Friday" -> 5;
+            case "Суббота", "Saturday" -> 6;
+            case "Воскресенье", "Sunday" -> 7;
             default -> -1;
         };
     }
