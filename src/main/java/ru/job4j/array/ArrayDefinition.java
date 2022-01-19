d2e7cfb12ab8a224384f0ab1352a41e4076bae90
kamyshenJava <kamyshen@tutanota.com>
2022-01-19 09:33:15 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "John";
+        names[1] = "Bob";
+        names[2] = "Frank";
+        names[3] = "Jack";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
